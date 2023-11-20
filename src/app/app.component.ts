01d0cceb243c5ff89e985eba6d02bd1e8ed017b4
sz-robin <saniuzzamanrobin07@gmail.com>
2023-11-20 23:32:15 +0600

    create pie chart UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ExpenseTabComponent } from "./shared/components/expense-tab/expense-tab.component";
+import { ExpensePieChartComponent } from "./shared/components/expense-pie-chart/expense-pie-chart.component";
+import { ExpenseTypeComponent } from "./shared/components/expense-type/expense-type.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, ExpenseTabComponent, ExpensePieChartComponent, ExpenseTypeComponent]
 })
 export class AppComponent {
   title = 'expense-chart';
